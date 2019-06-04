6e68d625f0a400d9b38199842726b31818731b18
Megha Prasad Bindiganavale <Megha@Meghas-MacBook-Pro.local>
2019-06-03 19:43:32 -0700

    append questions to the page when start is clicked

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 window.onload = function() {
-    $("#start").click(start);
+$("#start").click(start);
 }  
 var number = 100;
 var intervalId;
@@ -9,6 +9,9 @@ function start() {
         intervalId = setInterval(decrement, 1000);
         isClockRunning = true;
     }
+    $("#questions").append("<p>Which of the following vegetables is not one of the ingredients of V-8 juice?</p>");
+    $("#questions").append("<p>What country produces the most potatoes?</p>");
+    $("#questions").append("<p>According to a 1980s Beverage Media poll of four hundred bartenders, what was the female customers favorite drink?</p>");
 }
 function decrement(){
     number--;
